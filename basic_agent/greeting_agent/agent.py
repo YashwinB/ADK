ecf1730cff69eabf70dec57ace92a0cdbe52b450
YashwinB <yashwinb07@gmail.com>
2025-08-01 21:20:31 +0530

    init.py added

--- basic_agent/greeting_agent/agent.py
+++ basic_agent/greeting_agent/agent.py
@@ -4,6 +4,6 @@ root_agent = Agent(
     name="greeting_agent",
     model="gemini-2.0-flash",
     description="Greeting Agent",
-    instructions="""You are a helpful assistant that greets and welcomes the user.
-                    Ask for the user's name and greet them by their name."""
+    instruction="""You are a helpful assistant that greets and welcomes the user.
+                    Ask for the user's name and greet them by their name.""",
 )
